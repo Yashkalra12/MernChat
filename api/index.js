735640ea88fc79e14341734af2a8aa626eaa9bea
Yashkalra12 <yash1578.be21@chitkara.edu.in>
2024-02-26 17:53:33 +0530

    Register Page Completed

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const connectDB=require('./models/Connect');
 const User=require('./models/User');
 const jwt=require('jsonwebtoken');
 const cors=require('cors');
+const cookieParser =require('cookie-parser');
+
 
  dotenv.config();
 // mongoose.connect(process.env.MONGO_URL);
@@ -13,21 +15,37 @@ jwtSecret=process.env.JWT_SECRET;
 
 const app=express();
 app.use(express.json());
+app.use(cookieParser());
 
-// app.use(cors({
-//     credentials:true,
-//     origin:'*',
-//     methods:['GET','POST','PUT','DELETE']
-//     }));
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
 
+app.use(cors({
+    credentials:true,
+    origin:'*',
+    }));
 
 app.get('/test', (req,res)=>{
     res.json("Test okay");
 });
+
+
+app.get('/profile',(req,res)=>{
+    const token =req.cookies?.token;
+    if(token){
+        jwt.verify(token , jwtSecret , {} , (err,userData)=>{
+            if(err) throw err;
+            res.json(userData);
+        });
+    }
+    else{
+        res.status(401).json('no token');
+    }
+    
+
+
+});
+
+
+
 
 app.post('/register', async (req,res)=>{
     const {username, password}=req.body;
@@ -36,18 +54,19 @@ app.post('/register', async (req,res)=>{
         jwt.sign({userId: createdUser._id }, jwtSecret,{},(err,token) =>{
             if(err) throw err;
             res.cookie('token',token).status(201).json({
-                _id: createdUser._id,
-
+                id: createdUser._id,
+                username,
             });
         });
     }
         catch(err){
             if(err) throw err;
-            res.status(500).json('error');
+            console.log(err.message);
+            res.status(500).json({
+                message:err.message
+            })
         }
     }
-    
-
 );
 
 
